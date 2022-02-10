1d8e59686e22d9a67972fabb05b0f4dfc174a5ec
Sea <754119493@qq.com>
2022-02-10 15:42:56 +0800

    nacos config

--- service-consumer/src/main/java/com/sea/service/consumer/controller/ConsumerController.java
+++ service-consumer/src/main/java/com/sea/service/consumer/controller/ConsumerController.java
@@ -2,11 +2,17 @@ package com.sea.service.consumer.controller;
 
 import com.sea.service.consumer.service.ProviderService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RefreshScope
 public class ConsumerController {
+
+    @Value("${useLocalCache:false}")
+    private boolean useLocalCache;
 
     @Autowired
     ProviderService providerService;
@@ -14,5 +20,11 @@ public class ConsumerController {
     @GetMapping("/hi-feign")
     public String hiFeign() {
         return providerService.hi("feign");
+    }
+
+
+    @GetMapping("/getProp")
+    public boolean getProp() {
+        return useLocalCache;
     }
 }
